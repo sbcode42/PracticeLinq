4522decb5108f83f4db72cf1143b868928a98fc6
Sam Booth <ballersabooth@yahoo.com>
2020-09-10 12:00:42 -0500

    finished practice. add loop for console print

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameList = new List<string> { "The Sims", "Fallout", "World of Warcraft", "Beat Saber" };
+
+            var gameOrderByLength = gameList.OrderBy(game => game.Length);
+
+            Console.ReadLine();
+
+            //use a for each loop to print to console. DO THAT NEXT
+        }
+    }
+}
